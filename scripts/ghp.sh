59288a72f6f2775c9654d5623c1ee7eba942b0df
Martin Wragg <mdwragg@gmail.com>
2017-04-19 12:19:10 -0700

    Remove —force-latest tweak from the deploy scripts - not needed now we’re behind the prerelease tags.

--- scripts/ghp.sh
+++ scripts/ghp.sh
@@ -79,13 +79,13 @@ echo ${meta_temp/'COMPONENT_NAME'/$REPO_NAME} > index.html
 npm install bower -g
 bower cache clean
 # Install the repo and the dark-theme.
-bower install ${REPO_NAME} px-dark-theme px-dark-demo-theme --force-latest
+bower install ${REPO_NAME} px-dark-theme px-dark-demo-theme
 
 #copy the bower file into our root
 yes | cp ${REPO_NAME}/bower.json bower.json
 
 #and run install
-bower install --force-latest
+bower install
 
 # ------------------------------------------------------------------------------
 # BUILD PROJECT
